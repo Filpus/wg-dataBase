9f0008dd79f1ec00d2933df06829a6aa1ef8da0d
wkrzos <w.krzos.contact@gmail.com>
2024-10-27 23:09:50 +0100

    ref(phase1): move CHECKs under corresponding tables

--- initiate-database.sql
+++ initiate-database.sql
@@ -1,34 +1,90 @@
 -- Table definitions
 CREATE TABLE Cultures (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL UNIQUE, PRIMARY KEY (id));
+
 CREATE TABLE Religions (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL UNIQUE, PRIMARY KEY (id));
+
 CREATE TABLE SocialGroups (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL UNIQUE, baseSatisfaction float4 DEFAULT 0.5 NOT NULL, volunteers int4 DEFAULT 500 NOT NULL, PRIMARY KEY (id));
+ALTER TABLE SocialGroups 
+ADD CONSTRAINT base_satisfaction_check CHECK (baseSatisfaction >= 0 AND baseSatisfaction <= 1),
+ADD CONSTRAINT volunteers_check CHECK (volunteers >= 0);
+
 CREATE TABLE Populations (id SERIAL NOT NULL, fk_SocialGroups int4 NOT NULL, fk_Cultures int4 NOT NULL, fk_Religions int4 NOT NULL, fk_Locations int4 NOT NULL, satisfaction float4 NOT NULL, PRIMARY KEY (id));
+ALTER TABLE Populations 
+ADD CONSTRAINT satisfaction_check CHECK (satisfaction >= 0 AND satisfaction <= 1);
+
 CREATE TABLE Locations (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL UNIQUE, "size" int4 NOT NULL, fortifications int4 DEFAULT 0 NOT NULL, fk_Nations int4 NOT NULL, PRIMARY KEY (id));
+ALTER TABLE Locations 
+ADD CONSTRAINT size_check CHECK ("size" > 0),
+ADD CONSTRAINT fortifications_check CHECK (fortifications >= 0);
+
 CREATE TABLE LocationResources (id SERIAL NOT NULL, fk_Locations int4 NOT NULL, fk_Resources int4 NOT NULL, amount int4 NOT NULL, PRIMARY KEY (id));
+ALTER TABLE LocationResources
+ADD CONSTRAINT location_resources_amount_check CHECK (amount >= 0);
+
 CREATE TABLE Resources (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL UNIQUE, isMain BOOLEAN DEFAULT FALSE NOT NULL, PRIMARY KEY (id));
+
 CREATE TABLE Armies (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, fk_Nations int4 NOT NULL, fk_Locations int4 NOT NULL, PRIMARY KEY (id));
+
 CREATE TABLE Troops (id SERIAL NOT NULL, quantity int4 NOT NULL, fk_Armies int4 NOT NULL, fk_UnitTypes int4 NOT NULL, PRIMARY KEY (id));
+ALTER TABLE Troops 
+ADD CONSTRAINT quantity_check CHECK (quantity > 0);
+
 CREATE TABLE UnitTypes (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL UNIQUE, melee int4 NOT NULL, range int4 DEFAULT 0 NOT NULL, speed int4 NOT NULL, morale int4 NOT NULL, volunteersNeeded int4 NOT NULL, isNaval BOOLEAN NOT NULL, PRIMARY KEY (id));
+
 CREATE TABLE Nations (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL UNIQUE, fk_Cultures int4 NOT NULL, fk_Religions int4 NOT NULL, PRIMARY KEY (id));
+
 CREATE TABLE ProductionCosts (id SERIAL NOT NULL, fk_UnitTypes int4 NOT NULL, fk_Resources int4 NOT NULL, amount int4, PRIMARY KEY (id));
+ALTER TABLE ProductionCosts
+ADD CONSTRAINT amount_check CHECK (amount >= 0);
+
 CREATE TABLE OwnedResources (id SERIAL NOT NULL, fk_Nations int4 NOT NULL, fk_Resources int4 NOT NULL, amount int4 NOT NULL, PRIMARY KEY (id));
+ALTER TABLE OwnedResources
+ADD CONSTRAINT owned_resources_amount_check CHECK (amount >= 0);
+
 CREATE TABLE TradeAgreements (id SERIAL NOT NULL, fk_NationOffering int4 NOT NULL, fk_NationReceiving int4 NOT NULL, isAccepted BOOLEAN NOT NULL, PRIMARY KEY (id));
+
 CREATE TABLE Events (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255), PRIMARY KEY (id));
+
 CREATE TABLE Modifiers (id SERIAL NOT NULL, fk_Events int4 NOT NULL, target VARCHAR(255) NOT NULL, fk_Resources int4, fk_SocialGroups int4, fk_Cultures int4, fk_Religions int4, PRIMARY KEY (id));
 COMMENT ON COLUMN Modifiers.target IS 'Enum {Zadowolenie,Produkcja}';
+
 CREATE TABLE Users (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL UNIQUE, email VARCHAR(255) NOT NULL UNIQUE, password VARCHAR(255) NOT NULL, isArchived BOOLEAN NOT NULL, PRIMARY KEY (id));
+
 CREATE TABLE AccessesToNations (id SERIAL NOT NULL, fk_Nations int4 NOT NULL, fk_Users int4 NOT NULL, dateAcquired date, isActive BOOLEAN DEFAULT TRUE NOT NULL, PRIMARY KEY (id));
+
 CREATE TABLE Permissions (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL UNIQUE, PRIMARY KEY (id));
+
 CREATE TABLE GrantedPermissions (id SERIAL NOT NULL, fk_Users int4 NOT NULL, fk_Permissions int4 NOT NULL, PRIMARY KEY (id));
+
 CREATE TABLE Actions (id SERIAL NOT NULL, fk_Nations int4 NOT NULL, name VARCHAR(255), description VARCHAR(25000) NOT NULL, result VARCHAR(25000), isSettled BOOLEAN DEFAULT FALSE NOT NULL, PRIMARY KEY (id));
+
 CREATE TABLE UnitOrders (id SERIAL NOT NULL, fk_Nations int4 NOT NULL, fk_UnitTypes int4 NOT NULL, quantity int4 DEFAULT 1 NOT NULL, PRIMARY KEY (id));
+ALTER TABLE UnitOrders
+ADD CONSTRAINT unit_orders_quantity_check CHECK (quantity >= 0);
+
 CREATE TABLE AccessesToUnits (id SERIAL NOT NULL, fk_Nations int4 NOT NULL, fk_UnitTypes int4 NOT NULL, PRIMARY KEY (id));
+
 CREATE TABLE OfferedResources (id SERIAL NOT NULL, fk_Resources int4 NOT NULL, fk_TradeAgreements int4 NOT NULL, amount int4 NOT NULL, PRIMARY KEY (id));
+ALTER TABLE OfferedResources
+ADD CONSTRAINT offered_resources_amount_check CHECK (amount >= 0);
+
 CREATE TABLE WantedResources (id SERIAL NOT NULL, fk_Resources int4 NOT NULL, fk_TradeAgreements int4 NOT NULL, amount int4 NOT NULL, PRIMARY KEY (id));
+ALTER TABLE WantedResources
+ADD CONSTRAINT wanted_resources_amount_check CHECK (amount >= 0);
+
 CREATE TABLE RelatedEvents (id SERIAL NOT NULL, fk_Nations int4 NOT NULL, fk_Events int4 NOT NULL, PRIMARY KEY (id));
+
 CREATE TABLE UsedResources (id SERIAL NOT NULL, fk_SocialGroups int4 NOT NULL, fk_Resources int4 NOT NULL, amount int4 NOT NULL, PRIMARY KEY (id));
+ALTER TABLE UsedResources
+ADD CONSTRAINT used_resources_amount_check CHECK (amount >= 0);
+
 CREATE TABLE ProductionShares (id SERIAL NOT NULL, fk_SocialGroups int4 NOT NULL, fk_Resources int4 NOT NULL, coefficient float4 NOT NULL, PRIMARY KEY (id));
+ALTER TABLE ProductionShares
+ADD CONSTRAINT coefficient_check CHECK (coefficient >= 0);
+
 CREATE TABLE MaintenanceCosts (id SERIAL NOT NULL, fk_UnitTypes int4 NOT NULL, fk_Resources int4 NOT NULL, amount int4 NOT NULL, PRIMARY KEY (id));
+ALTER TABLE MaintenanceCosts
+ADD CONSTRAINT maintenance_costs_amount_check CHECK (amount >= 0);
 
 -- Foreign keys
 ALTER TABLE Populations 
@@ -300,67 +356,6 @@ ADD CONSTRAINT fk_maintenanceCosts_resources
 FOREIGN KEY (fk_Resources)
 REFERENCES Resources(id)
 ON DELETE CASCADE;
-
--- Check constraints
-ALTER TABLE SocialGroups
-ADD CONSTRAINT base_satisfaction_check
-CHECK (baseSatisfaction >= 0 AND baseSatisfaction <= 1);
-
-ALTER TABLE SocialGroups
-ADD CONSTRAINT volunteers_check
-CHECK (volunteers >= 0);
-
-ALTER TABLE Populations
-ADD CONSTRAINT satisfaction_check
-CHECK (satisfaction >= 0 AND satisfaction <= 1);
-
-ALTER TABLE Locations
-ADD CONSTRAINT size_check
-CHECK ("size" > 0);
-
-ALTER TABLE Locations
-ADD CONSTRAINT fortifications_check
-CHECK (fortifications >= 0);
-
-ALTER TABLE Troops
-ADD CONSTRAINT quantity_check
-CHECK (quantity > 0);
-
-ALTER TABLE ProductionCosts
-ADD CONSTRAINT amount_check
-CHECK (amount >= 0);
-
-ALTER TABLE OwnedResources
-ADD CONSTRAINT owned_resources_amount_check
-CHECK (amount >= 0);
-
-ALTER TABLE LocationResources
-ADD CONSTRAINT location_resources_amount_check
-CHECK (amount >= 0);
-
-ALTER TABLE OfferedResources
-ADD CONSTRAINT offered_resources_amount_check
-CHECK (amount >= 0);
-
-ALTER TABLE WantedResources
-ADD CONSTRAINT wanted_resources_amount_check
-CHECK (amount >= 0);
-
-ALTER TABLE UsedResources
-ADD CONSTRAINT used_resources_amount_check
-CHECK (amount >= 0);
-
-ALTER TABLE MaintenanceCosts
-ADD CONSTRAINT maintenance_costs_amount_check
-CHECK (amount >= 0);
-
-ALTER TABLE UnitOrders
-ADD CONSTRAINT unit_orders_quantity_check
-CHECK (quantity >= 0);
-
-ALTER TABLE ProductionShares
-ADD CONSTRAINT coefficient_check
-CHECK (coefficient >= 0);
 
 -- UniqueTogether constraints
 ALTER TABLE TradeAgreements
